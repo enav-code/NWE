a4858474eed08bc947c1a6528c575b2da11a6365
enav <evank.ebe12@gmail.com>
2026-06-04 19:24:19 -0400

    Add Config.py

--- Config.py
+++ Config.py
@@ -21,8 +21,8 @@ DEBUG = FLASK_ENV != "production"
 # GOOGLE SSO
 # =========================================================
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "")
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "")
+GOOGLE_CLIENT_ID = os.environ.get("77051269767-iv2mkdt5vhlpcuqookcqib1ssiqtc0oc.apps.googleusercontent.com", "")
+GOOGLE_CLIENT_SECRET = os.environ.get("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", "")
 
 GOOGLE_REDIRECT_URI = os.environ.get(
     "GOOGLE_REDIRECT_URI",
